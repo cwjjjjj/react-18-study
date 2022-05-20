b828cd7c4beb47f0761cd2295f9bfaa16d0e3d43
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2022-05-20 17:41:13 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { css } from "@emotion/react";
+
 function App() {
-  return <div>react</div>;
+  return (
+    <div
+      // className=" text-red-300"
+      css={css`
+        font-size: 100px;
+        color: red;
+      `}
+    >
+      react
+    </div>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  resolve: {
+    alias: [
+      // { find: "@", replacement: path.resolve(__dirname, "./src") },
+      { find: /^~/, replacement: "" },
+    ],
+  },
+  esbuild: {
+    jsxFactory: "jsx",
+    jsxInject: `
+      import React from 'react'  
+      import { jsx } from '@emotion/react'
+    `,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          return id.includes("node_modules") ? "vendor" : "common";
+        },
+      },
+    },
+  },
+});
