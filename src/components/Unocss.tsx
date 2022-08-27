32b74950c2420c444c311cb560f7b0990fc23280
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2022-08-27 10:48:51 +0800

    test: 💍 test z-index

--- src/components/Unocss.tsx
+++ src/components/Unocss.tsx
@@ -1,7 +1,31 @@
+import { css } from "@emotion/react";
+
 export default function Unocss() {
   return (
     <div hover="color-blue" className=" text-4xl color-red">
       Unocss123123
+      <div
+        css={css`
+          background-color: red;
+          height: 200px;
+          width: 100px;
+          position: absolute;
+        `}
+      >
+        1
+      </div>
+      <div
+        css={css`
+          background-color: orange;
+          height: 200px;
+          width: 100px;
+          display: flex;
+          /* position: relative; */
+          z-index: 10;
+        `}
+      >
+        2
+      </div>
     </div>
   );
 }
