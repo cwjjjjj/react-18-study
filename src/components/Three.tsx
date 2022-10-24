623fb660afcbc8ec639b26061acc1ac6be20f27e
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2022-10-24 10:40:00 +0800

    feat: 🎸 three plugin

--- src/components/Three.tsx
+++ src/components/Three.tsx
@@ -1,34 +1,44 @@
 import * as THREE from "three";
+import { useEffect, useRef } from "react";
 
 export default function Three() {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
+  const ref = useRef<HTMLDivElement>(null);
+  const init = () => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    if (ref.current) {
+      ref.current.append(renderer.domElement);
+    }
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-  camera.position.z = 5;
+    camera.position.z = 5;
 
-  function animate() {
-    requestAnimationFrame(animate);
+    function animate() {
+      requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
 
-    renderer.render(scene, camera);
-  }
+      renderer.render(scene, camera);
+    }
 
-  animate();
-  return <div>Three</div>;
+    animate();
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+  return <div ref={ref}>Three</div>;
 }
